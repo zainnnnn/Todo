a997af524ac9d92da6e833cf2aca20da4991c77c
zainnnnn <mzain2353@gmail.com>
2024-01-04 12:29:28 +0100

    Done django headers setup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5173"
+]
+
 
 # Application definition
 
@@ -37,11 +41,17 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # External
+    "corsheaders",
+    "restframework",
+    # Internal
+    "todo",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'corsheaders.middleware.CorsMiddleware'
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
