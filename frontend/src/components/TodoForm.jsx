21d4a3fc1505f5db58df66f1fe8f3c6d39fa5c3c
zainnnnn <mzain2353@gmail.com>
2024-01-09 15:23:25 +0100

    Adding Crud Functionalities

--- frontend/src/components/TodoForm.jsx
+++ frontend/src/components/TodoForm.jsx
@@ -1,9 +1,28 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
 const TodoForm = () => {
+  const [newTodo, setNewTodo] = useState({
+    body: "",
+  });
+
+  const handleChange = (e) => {
+    setNewTodo((prev) => ({
+      ...prev,
+      body: e.target.value,
+    }));
+    console.log(newTodo);
+  };
+
   return (
     <div className="d-flex justify-content-center">
-      <input type="text" placeholder="Type here" className="form-control" />
+      <input
+        type="text"
+        placeholder="Type here"
+        className="form-control"
+        onChange={handleChange}
+        value={newTodo.body}
+      />
       <button className="btn btn-primary ml-2">Add TODO</button>
     </div>
   );
