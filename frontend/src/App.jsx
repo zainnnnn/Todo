490db19fb81e870512c8cb74e44904ed8001093d
zainnnnn <mzain2353@gmail.com>
2024-01-09 14:10:30 +0100

    Integrated the Api with frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,34 +2,41 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import Table from "./components/Table";
 import TodoForm from "./components/TodoForm";
-import axios from "axios";
+import axios, { CanceledError } from "axios";
 
 function App() {
   const [todos, setTodos] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
+  // const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetchData();
-    console.log(todos);
-    setIsLoading(false);
+    const controller = new AbortController();
+    axios
+      .get("http://localhost:8000/api/todo/", { signal: controller.signal })
+      .then((res) => {
+        setTodos(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        // setError(err.message);
+        setLoading(false);
+      });
+    return () => controller.abort();
   }, []);
-
-  const fetchData = async () => {
-    try {
-      const response = await axios.get("http://localhost:8000/api/todo/");
-      console.log(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <div className="bg-indigo-200 px-8 min-h-screen">
       <nav className="pt-8">
         <h1 className="text-5xl text-center pb-12">ToDo List</h1>
       </nav>
       <TodoForm />
-      <Table todos={todos} setTodos={setTodos} isLoading={isLoading} />
+      <Table
+        todos={todos}
+        setTodos={setTodos}
+        isLoading={isLoading}
+        // error={error}
+        f
+      />
     </div>
   );
 }
--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -1,11 +1,14 @@
 import {
-  //   MdOutlineCheckBoxOutlineBlank,
+  MdOutlineCheckBoxOutlineBlank,
   MdOutlineDeleteOutline,
   MdEditNote,
   MdOutlineCheckBox,
 } from "react-icons/md";
 
-const Table = ({ todos, setTodos, isLoading }) => {
+const Table = (Props) => {
+  const todos = Props.todos;
+  const isLoading = Props.isLoading;
+
   return (
     <div className="py-2 ">
       <table className="w-11/12 max-w-4xl">
@@ -29,35 +32,51 @@ const Table = ({ todos, setTodos, isLoading }) => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td className="p-3 text-sm">
-              <span className="inline-block cursor-pointer">
-                <MdOutlineCheckBox />
-              </span>
-            </td>
-            <td className="p-3 text-sm">
-              nemo aperiam magnam et similique quaerat molestias dignissimos
-            </td>
-            <td className="p-3 text-sm text-center">
-              {" "}
-              <span className="p-1.5 text-xs font-medium tracking-wider rounded-md bg-green-300 ">
-                Done
-              </span>
-            </td>
-            <td className="p-3 text-sm">22-04-23</td>
-            <td className="p-3 text-sm font-medium grid grid-flow-col items-center mt-5">
-              <span className="text-xl cursor-pointer">
-                <MdEditNote />
-              </span>
-              <span className="text-xl cursor-pointer">
-                <MdOutlineDeleteOutline />
-              </span>
-            </td>
-          </tr>
+          {isLoading ? (
+            <div> Is isLoading </div>
+          ) : (
+            <>
+              {todos.map((todoItem) => {
+                return (
+                  <tr key={todoItem.id} className="border-b border-black ">
+                    <td className="p-3 text-sm" title={todoItem.id}>
+                      <span className="inline-block cursor-pointer">
+                        {todoItem.completed ? (
+                          <MdOutlineCheckBox />
+                        ) : (
+                          <MdOutlineCheckBoxOutlineBlank />
+                        )}
+                      </span>
+                    </td>
+                    <td className="p-3 text-sm">{todoItem.body}</td>
+                    <td className="p-3 text-sm text-center">
+                      {" "}
+                      <span className="p-1.5 text-xs font-medium tracking-wider rounded-md bg-green-300 ">
+                        Done
+                      </span>
+                    </td>
+                    <td className="p-3 text-sm">22-04-23</td>
+                    <td className="p-3 text-sm font-medium   items-center mt-5">
+                      <span className="text-xl cursor-pointer">
+                        <MdEditNote />
+                      </span>
+                      <span className="text-xl cursor-pointer">
+                        <MdOutlineDeleteOutline />
+                      </span>
+                    </td>
+                  </tr>
+                );
+              })}
+            </>
+          )}
         </tbody>
       </table>
     </div>
   );
 };
+
+// Table.propTypes = {
+//   todos: PropTypes.array.isRequired,
+// };
 
 export default Table;
